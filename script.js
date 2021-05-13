5eafc588147884dc684f6f66353f611c23fae311
Jason Li <jasonli.0616@outlook.com>
2021-05-13 08:56:14 -0400

    Add reset button

--- script.js
+++ script.js
@@ -10,4 +10,12 @@ function inactive() {
 function active() {
     winIsActive = true;
     p.innerHTML = `<br>You switched to a different tab ${timesSwitched} times`;
+    if (timesSwitched == 1) {
+        p.innerHTML = `<br>You switched to a different tab ${timesSwitched} time`;
+    }
+}
+
+function reset() {
+    timesSwitched = 0;
+    p.innerHTML = `<br>You switched to a different tab 0 times`;
 }
