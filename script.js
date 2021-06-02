3169ef53b35f84f0913dc83297f51ca73b20c0ef
Jason Li <jasonli.0616@outlook.com>
2021-06-02 15:13:05 -0400

    Make mobile text visible on mobile devices only

--- script.js
+++ script.js
@@ -19,3 +19,7 @@ function reset() {
     timesSwitched = 0;
     p.innerHTML = `<br>You switched to a different tab 0 times`;
 }
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    document.getElementById('mobileText').style.visibility = 'visible';
+}
